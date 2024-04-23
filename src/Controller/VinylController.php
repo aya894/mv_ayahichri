4dced7f8e0cdfeb8a466558f5fae3fe718a32f2b
Aya Hichri <ayahichri2022@gmail.com>
2024-04-23 11:25:08 +0200

    Custom Entity Methods & Twig Magic

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\VinylMixRepository;
-use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +11,7 @@ use function Symfony\Component\String\u;
 class VinylController extends AbstractController
 {
     public function __construct(
-        private bool $isDebug,
-        private MixRepository $mixRepository
+        private bool $isDebug
     )
     {}
 
