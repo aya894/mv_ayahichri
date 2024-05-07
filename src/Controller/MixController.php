2e77b5d974db7162a0c38496cf9a1afd876902c5
Aya Hichri <ayahichri2022@gmail.com>
2024-05-07 16:49:47 +0200

    Clean URLs with Sluggable

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MixController extends AbstractController
 {
@@ -18,26 +19,25 @@ class MixController extends AbstractController
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
-        $genres = ['pop', 'rock'];
-        $mix->setGenre($genres[array_rand($genres)]);
+        $mix->setGenre('pop');
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
-
         $entityManager->persist($mix);
         $entityManager->flush();
-
         return new Response(sprintf(
             'Mix %d is %d tracks of pure 80\'s heaven',
             $mix->getId(),
             $mix->getTrackCount()
         ));
+        $genres = ['pop', 'rock'];
+        $mix->setGenre($genres[array_rand($genres)]);
     }
-
-    #[Route('/mix/{id}', name: 'app_mix_show')]
+    //#[Route('/mix/{id}')]
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
     public function show(VinylMix $mix): Response
     {
         return $this->render('mix/show.html.twig', [
-            'mix' => $mix,
+            'slug' => $mix->getSlug(),
         ]);
     }
     #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
@@ -52,7 +52,7 @@ class MixController extends AbstractController
         $entityManager->flush();
         $this->addFlash('success', 'Vote counted!');
         return $this->redirectToRoute('app_mix_show', [
-            'id' => $mix->getId(),
+            'slug' => $mix->getSlug(),
         ]);
     }
 }
